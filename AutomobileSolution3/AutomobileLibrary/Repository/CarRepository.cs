11e786a20673b1b3955228691ad0aec7dbab911f
MarkMinh <minh8adv@gmail.com>
2024-03-08 08:14:17 +0700

    Lab 3

---
+++ AutomobileSolution3/AutomobileLibrary/Repository/CarRepository.cs
@@ -0,0 +1,28 @@
+﻿using AutomobileLibrary.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileLibrary.Repository
+{
+    public class CarRepository : ICarRepository
+    {
+        CarDAO carDAO = CarDAO.Instance;    
+        public void DeleteCar(int carId)
+        => carDAO.Remove(carId);
+
+        public Car GetCarByID(int carId)
+        => carDAO.GetCarByID(carId);
+
+        public IEnumerable<Car> GetCars()
+        => carDAO.GetCarList();
+
+        public void InsertCar(Car car)
+        => carDAO.AddNew(car);
+
+        public void UpdateCar(Car car)
+        => carDAO.Update(car);
+    }
+}
